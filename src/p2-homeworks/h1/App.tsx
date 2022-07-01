775468487412dbb80bd05eb1ff11fd69c66c27c0
irina-lat <irochka.bars@mail.ru>
2022-07-01 20:15:15 +0300

    remove file

--- src/p2-homeworks/h1/App.tsx
+++ src/p2-homeworks/h1/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import s from '../../p1-main/m1-ui/u1-app/App.module.css'
 import HW1 from './HW1'
-import HW2 from "./HW2";
-import HW3 from "./HW3";
-import HW4 from "./HW4";
-import HW5 from "./HW5";
 
 
 
@@ -14,13 +10,6 @@ function App() {
         <div className={s.App}>
             <div>react homeworks:</div>
             <HW1/>
-            <HW2/>
-            <HW3/>
-            <HW4/>
-            <HW5/>
-
-
-
         </div>
     )
 }
