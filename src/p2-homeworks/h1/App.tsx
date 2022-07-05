1c5f2458b466510273a65b97bcf75dd1eb779663
irina-lat <irochka.bars@mail.ru>
2022-07-06 01:53:20 +0300

    dz №2

--- src/p2-homeworks/h1/App.tsx
+++ src/p2-homeworks/h1/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import s from '../../p1-main/m1-ui/u1-app/App.module.css'
 import HW1 from './HW1'
-
+import HW2 from "../h2/HW2";
 
 
 
@@ -10,6 +10,7 @@ function App() {
         <div className={s.App}>
             <div>react homeworks:</div>
             <HW1/>
+            <HW2/>
         </div>
     )
 }
