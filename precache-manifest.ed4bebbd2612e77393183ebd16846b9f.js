0690aa39dbf128695708805210f9952a1ef5efe6
irina-lat <irochka.bars@mail.ru>
2022-07-28 16:31:39 +0300

    Updates

---
+++ precache-manifest.ed4bebbd2612e77393183ebd16846b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4213a2ed541557bfa5b8fe23529d5d2b",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "b47781cd5db6004b9f41",
+    "url": "/simple-react-homeworks/static/css/main.41a7abc7.chunk.css"
+  },
+  {
+    "revision": "aec7f5e25eac78174668",
+    "url": "/simple-react-homeworks/static/js/2.e32f1ae6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-react-homeworks/static/js/2.e32f1ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47781cd5db6004b9f41",
+    "url": "/simple-react-homeworks/static/js/main.a50bc2b6.chunk.js"
+  },
+  {
+    "revision": "35a9557543d5cb14c0c5",
+    "url": "/simple-react-homeworks/static/js/runtime-main.5a451111.js"
+  }
+]);
